f41d7d57469e347b3d3589f5782fd5215a4263d8
Lars Tackmann <ltackmann@gmail.com>
2013-09-30 10:32:55 +0200

    added iOS project

---
+++ iOS/iPad/solvr_ipadapp.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2013 Solvr, Inc. All rights reserved.
+//
+// This is commercial software. Use or redistribution of this code in full 
+// or in part without the express written consent of Solvr is prohibited.
+
+library solvr_ipadapp;
+
+import 'package:solvr_ios/solvr_ios.dart';
+
