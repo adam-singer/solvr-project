dda2a9029b13d38af33c17913ac75bd827a4a067
Lars Tackmann <ltackmann@gmail.com>
2014-03-15 10:46:55 +0100

    rename test suite and optimize it

---
+++ run_tests.sh
@@ -0,0 +1,23 @@
+declare -a projects=(
+	"common:solvr_common_test.dart" 
+	"types:skip" 
+	"ast:solvr_ast_test.dart" 
+	"parser:solvr_parser_test.dart" 
+	"type_checker:solvr_type_checker_test.dart" 
+	"kernel:solvr_kernel_internals_test.dart")
+
+# TODO run all kernel tests
+
+# execute tests
+for project in "${projects[@]}" ; do
+    project_dir=${project%%:*}
+    test_suite=${project#*:}
+    if [ "$test_suite" != "skip" ]; then 
+    	pushd "$project_dir"
+    		printf "Testing project [%s] using suite [%s]\n" "$project_dir" "$test_suite"
+    		dart "test/$test_suite"
+    	popd
+   	fi
+done
+
+
