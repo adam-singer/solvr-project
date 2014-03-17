cf3b4445d48a4f5745d5b2e076ddad2db155d20d
Lars Tackmann <ltackmann@gmail.com>
2014-03-17 19:11:59 +0100

    rename

---
+++ backend/lib/solvr_backend.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2013, the Solvr project authors. Please see the AUTHORS 
+// file for details. All rights reserved. Use of this source code is 
+// governed by a Apache license that can be found in the LICENSE file.
+
+library solvr_backend;
+
+import "package:log4dart/log4dart.dart";
+
+// TODO replay commands from notebook against the backend (use the file event store to dump 
+// then as JSON and fire this JSON against the backend HTTP server)
+
