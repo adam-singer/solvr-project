f7261bf18e401bb2968a4da7247ea89f52feb930
Lars Tackmann <ltackmann@gmail.com>
2014-03-27 15:19:42 +0100

    clarify dependencies

--- run_pub.sh
+++ run_pub.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-declare -a projects=("common" "types" "ast" "parser" "analyzer" "kernel" "logic" "ui" "site" "webapp")
+# projects ordered by their dependencies
+declare -a projects=(
+	"common" 
+	"types"     # depends on common
+	"ui"        # depends on common
+	"site"      # depends on ui, common
+	"ast"       # depends on types, common
+	"parser"    # depends on ast, types, common
+	"analyzer"  # depends on parser, ast, types, common
+	"kernel"    # depends on analyzer, parser, ast, types, common
+	"logic"     # depends on kernel, analyzer, parser, ast, types, common
+	"webapp")   # depends on ui, logic, kernel, analyzer, parser, ast, types, common
 
 for i in "${projects[@]}"
 do
